53bb0a908c90f8ab09fcc35c12ff6947ea7a5ca5
Aondohemba <akighirjoseph77@gmail.com>
2023-05-19 16:38:08 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const searchBarContainerEl = document.querySelector(".search-bar-container");
+const magnifierEl = document.querySelector(".magnifier");
+
+magnifierEl.addEventListener("click", ()=>{
+    searchBarContainerEl.classList.toggle("active");
+});
